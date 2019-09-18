569a81e2895e7359c60f179a676e6e425b72ee99
Damini Varu <damvaru4@gmail.com>
2019-09-18 01:57:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,3 +11,14 @@ end
 Daisy = Dog.new("Daisy")
 Daisy.name = "Daisy"
 
+  def initialize(breed)
+    @breed = breed 
+  end 
+  
+  def breed
+    @breed
+  end 
+end 
+
+pug = Dog.new("pug")
+pug.breed = "pug"
